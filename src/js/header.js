b82fb14adb81073a386b621c82c50a99a3e8c604
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2024-11-16 14:41:28 +0100

    onCheck

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,13 @@
 const button = document.querySelector('.headerButton');
 const mobMenu = document.querySelector('.mobMenu');
 const icon = document.querySelector('.iconUse');
+const link = './img/sprite.svg#icon-close';
 
 const toggleMenu = () => {
   const isOpen = mobMenu.classList.toggle('isOpen');
   if (isOpen) {
     document.body.style.overflow = 'hidden';
-    icon.setAttribute('href', './img/sprite.svg#icon-close');
+    icon.setAttribute('href', link);
   } else {
     document.body.style.overflow = '';
     icon.setAttribute('href', './img/sprite.svg#icon-burger');
