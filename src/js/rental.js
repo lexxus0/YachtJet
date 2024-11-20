1b4c106f53e311851a2adae9f60684f28342f024
Yurii <y.tsyhykal@gmail.com>
2024-11-20 14:50:27 +0100

    done rental section

--- src/js/rental.js
+++ src/js/rental.js
@@ -6,11 +6,24 @@ const swiper = new Swiper('.rental-swiper', {
   loop: true,
   spaceBetween: 30,
   slidesPerView: 1,
-  autoplay: {
-    delay: 2000,
+  breakpoints: {
+    768: {
+      spaceBetween: 32,
+      slidesPerView: 2,
+    },
+    1280: {
+      enabled: false,
+      spaceBetween: 25,
+      slidesPerView: 3,
+    },
+    1440: {
+      enabled: false,
+      spaceBetween: 32,
+      slidesPerView: 3,
+    },
   },
-  centerInsufficientSlides: false,
-  centeredSlidesBounds: false,
-  grabCursor: false,
-  preventClicks: false,
+  autoplay: {
+    delay: 3000,
+  },
+  preventClicks: true,
 });
